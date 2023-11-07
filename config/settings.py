e78126dfe3ad1522a5e987d829c5483bee7a01da
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-11-07 15:51:48 +0330

    chore: acc settings

--- config/settings.py
+++ config/settings.py
@@ -7,12 +7,11 @@ class Settings(BaseSettings):
     secret_key: str
     jwt_algorithm: str
 
-    jwt_access_lifetime_min: int = 60 * 24 * 7
-    jwt_refresh_lifetime_min: int = jwt_access_lifetime_min * 10
-    jwt_token_prefix: str
-
     redis_host: str
     redis_port: str
+
+    MONGO_HOST: str
+    MONGO_PORT: str
 
     class Config:
         env_file = '.env'
