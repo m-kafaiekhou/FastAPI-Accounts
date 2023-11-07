a3c3698c621213bea7ca04263aee2831e3b2a08d
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-11-07 15:52:36 +0330

    fix: remove id from schema

--- schemas/authorization_schemas.py
+++ schemas/authorization_schemas.py
@@ -23,7 +23,6 @@ class CreateUserSchema(BaseModel):
 class UserSchema(CreateUserSchema):
     full_name: str | None = None 
     address: str | None = None
-    _id: str | None = None
 
     @validator('full_name')
     def fullname_validator(cls, v):
